6194788eb5a9c6d1cfbb8f457b365ae51fdb10c2
Yao Yue <thinkingfish@gmail.com>
2016-10-12 19:24:47 -0700

    add default push policy

--- git-setup.sh
+++ git-setup.sh
@@ -10,3 +10,5 @@ git config --global alias.br branch
 git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
 git config --global alias.type 'cat-file -t'
 git config --global alias.dump 'cat-file -p'
+
+git config --global push.default simple
